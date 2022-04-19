4f2520f490f2c264258f162b599dcb2d07340a67
Ankush Kumar <kumarankush1709@gmail.com>
2022-04-20 00:11:43 +0530

    starting frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,6 @@ import React from 'react'
 import Home from './components/Home'
 import Login from './components/Login'
 import Signup from './components/Signup'
-import Application from './components/Application'
-import Profile from './components/Profile'
-import LendMoney from './components/LendMoney'
-import YashTarun from './components/YashTarun'
-import User2 from './components/User2'
-import User3 from './components/User3'
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 const App = () => {
   return (
@@ -17,12 +11,6 @@ const App = () => {
        <Route path="login" element={<Login/>} />
        <Route path="signup" element={<Signup/>} />
        <Route path="/" element={<Home/>} />
-       <Route path="profile" element={<Profile/>} />
-       <Route path="application" element={<Application/>} />
-       <Route path="lendmoney" element={<LendMoney/>} />
-       <Route path="yashtarun" element={<YashTarun/>} />
-       <Route path="user2" element={<User2/>} />
-       <Route path="user3" element={<User3/>} />
        </Routes>
      </Router>
     </>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './components/Application.css'
 import './components/Style.css'
 import App from './App';
 // import {BrowserRouter} from 'react-router-dom';
