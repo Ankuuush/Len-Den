786126a3ddfbcb6074b3412d31af655ed62454df
Ankush Kumar <kumarankush1709@gmail.com>
2022-04-23 17:42:26 +0530

    Signup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/NavBar';
 import Login from './Components/Login';
+import Signup from './Components/Signup';
 import {
   BrowserRouter as Router,
   Routes,
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<NavBar/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
       </Routes>
     </Router>
   );
